8f4d07f2b55027293173adaf5f803d061890d376
unknown <ramyadeep@ncompass.in>
2022-06-13 12:21:58 +0530

    added more rows in the form

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const form = document.getElementById('my-form');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const userEmail = form.elements['email'].value; // by name
+    const fName = form.elements['fname'].value;
+    const lName = form.elements['lname'].value;
+    const userEmail = form.elements['email'].value;
     const userPassword = form.elements['password'].value;
     const userTable = document.getElementById('user-table');
     const row = userTable.insertRow();
-    // const serial = userTable.elements.length;
+    const serial = userTable.rows.length;
     console.log(userTable);
-    row.innerHTML = `<tr><td> ${userEmail}</><td>${userPassword}</td></tr> `;
+    row.innerHTML = `<tr><td>${serial-1}. ${fName}</><td>${lName}</td><td> ${userEmail}</><td>${userPassword}</td></tr> `;
 });
